1cbfdfa4086972d12427baf4374cf0a7137a6aa9
Sunny Manik <sundeepkmanik@gmail.com>
2018-07-31 00:23:20 -0700

    incorporating search

--- client/src/components/Search/Search.js
+++ client/src/components/Search/Search.js
@@ -1,19 +1,47 @@
-import React from "react";
+import React, { Component } from "react";
 import "./Search.css";
 
-const Search = () => (
-	<div>
-		<div className="field has-addons">
-		  <div className="control search-container">
-		    <input className="input" type="text" placeholder="Enter a facility name..." />
-		  </div>
-		  <div className="control">
-		    <a className="button is-info">
-		      Search
-		    </a>
-		  </div>
-		</div>
-	</div>
-);
+class Search extends Component {
+
+	state = {
+		input: "",
+	};
+
+	// input change handler
+	onInput = event => this.setState({
+		input: event.target.value
+	});
+
+	// submit handler
+	onSearch = event => {
+		event.preventDefault();
+		console.log("I added an on click!");
+		// this.props.history.push("/results?q=Hello");
+
+		this.props.onSubmit(this.state.input);
+	};
+
+	render() {
+		return (
+			<div>
+				<div className="field has-addons">
+					<div className="control search-container">
+						<input
+							className="input"
+							type="text"
+							placeholder="Enter a facility name..."
+							onChange={this.onInput}
+						/>
+					</div>
+					<div className="control">
+						<a onClick={this.onSearch} className="button is-info">
+							Search
+						</a>
+					</div>
+				</div>
+			</div>
+		);
+	};
+}
 
 export default Search;
--- client/src/pages/Results/Results.js
+++ client/src/pages/Results/Results.js
@@ -5,26 +5,67 @@ import Card from "../../components/Card";
 import API from "../../utils/API";
 import "./Results.css";
 
-const Results = () => (
-	<div>
-		<section className="hero is-primary">
-			<div className="hero-body">
-				<div className="container">
-					<h1 className="title">Results List</h1>
-					{/* A search bar like the one on the home page */}
-					<Search />
-				</div>
+class Results extends Component {
+	state = {};
+
+	componentDidMount() {
+		// API stuff
+		console.log("Results page!");
+
+	};
+
+	searchForBusiness = query => {
+		const searchPath = "/results?q=" + query;
+		const encodedPath = encodeURI(searchPath);
+
+		this.props.history.push(encodedPath);
+	};
+
+	render() {
+		return (
+			<div>
+				<section className="hero is-primary">
+					<div className="hero-body">
+						<div className="container">
+							<h1 className="title">Results List</h1>
+							{/* A search bar like the one on the home page */}
+							<Search onSubmit={this.searchForBusiness} />
+						</div>
+					</div>
+				</section>
+				<section className="main section">
+					<div className="container">
+						{/* Cards will be generated here, here's an example */}
+						<Card />
+						<Card />
+						<Card />
+					</div>
+				</section>
 			</div>
-		</section>
-		<section className="main section">
-			<div className="container">
-				{/* Cards will be generated here, here's an example */}
-				<Card />
-				<Card />
-				<Card />
-			</div>
-		</section>
-	</div>
-);
+		);
+	};
+}
+
+// // const Results = () => (
+// 	<div>
+// 		<section className="hero is-primary">
+// 			<div className="hero-body">
+// 				<div className="container">
+// 					<h1 className="title">Results List</h1>
+// 					{/* A search bar like the one on the home page */}
+// 					<Search />
+// 				</div>
+// 			</div>
+// 		</section>
+// 		<section className="main section">
+// 			<div className="container">
+// 				{/* Cards will be generated here, here's an example */}
+// 				<Card />
+// 				<Card />
+// 				<Card />
+// 			</div>
+// 		</section>
+// 	</div>
+// // );
 
 export default Results;
