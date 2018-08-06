9befb8232fe7b0acf2a0d7357d1fe943c46c7833
Sunny Manik <smanik@Sunnys-MacBook-Pro.local>
2018-08-05 21:13:55 -0700

    changing jquery to react

--- client/src/components/Carousel/Carousel.js
+++ client/src/components/Carousel/Carousel.js
@@ -3,9 +3,31 @@ import CarItem from "./CarItem.js";
 import "./Carousel.css";
 
 class Carousel extends Component {
-	state = {
+	state = {};
 
-	};
+	scrollRight = () => {
+		console.log("Going somewhere!");
+	}
+
+	scrollLeft = () => {
+		console.log("Going somewhere!");
+	}
+
+	sideScroll = (element, direction, speed, distance, step) => {
+		let scrollAmount = 0;
+
+		const slideTimer = setInterval(function(){
+			if (direction == "left") {
+				element.scrollLeft -= step;
+			} else {
+				element.scrollLeft += step;
+			}
+			scrollAmount += step;
+			if (scrollAmount >= distance) {
+				window.clearInterval(slideTimer);
+			}
+		}, speed);
+	}
 
 	render() {
 		return(
@@ -13,8 +35,26 @@ class Carousel extends Component {
 				<div className="cl-carouselwrap">
 					<div className="cl-carouselstrip">
 						{/* temporary, will put these arrow svgs in their own section later, just testing rn */}
-						<div className="cl-arrowbutton" id="cl-arrowright"></div>
-		          		<div className="cl-arrowbutton" id="cl-arrowleft"></div>
+						<div className="cl-arrowbutton" id="cl-arrowright" onClick={this.scrollRight}>
+							<svg xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink" version="1.1" x="0px" y="0px" width="100%" height="100%" viewBox="0 0 306 306" xmlSpace="preserve">
+								<g>
+									<g>
+										<polygon points="94.35,0 58.65,35.7 175.95,153 58.65,270.3 94.35,306 247.35,153">
+										</polygon>
+									</g>
+								</g>
+							</svg>
+						</div>
+		          		<div className="cl-arrowbutton" id="cl-arrowleft" onClick={this.scrollLeft}>
+							<svg xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink" version="1.1" x="0px" y="0px" width="100%" height="100%" viewBox="0 0 306 306" xmlSpace="preserve">
+								<g>
+									<g>
+										<polygon points="247.35,270.3 130.05,153 247.35,35.7 211.65,0 58.65,153 211.65,306">
+										</polygon>
+									</g>
+								</g>
+							</svg>
+		          		</div>
 		              	{/* temporary, will put these arrow svgs in their own section later, just testing rn */}
 
 		              	<CarItem />
--- controllers/facilitiesController.js
+++ controllers/facilitiesController.js
@@ -1,4 +1,4 @@
-const request = require('request');
+ const request = require('request');
 
 module.exports = {
   findAll: function (req, res) {
